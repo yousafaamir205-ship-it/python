c74ebfa3fb637e1ecf36ce69377c80ab82ee6193
MUHAMMAD YOUSAF <yousafaamir205@gamil.com>
2026-06-25 10:26:15 +0500

    first code

---
+++ first.py
@@ -0,0 +1,75 @@
+# n1=5
+# n2=2
+# sum=n1+n2
+# print(sum)
+# print(type(sum))
+# print(n1 / n2)
+# print(n1 % n2)
+# print(n1 ** n2)#power
+# print(n1 > n2)
+# print(n1 >= n2)
+# print(n1 < n2)
+# print(n1 <= n2)
+#assignment operators
+# a=50
+# a%=10
+# print(" a : ", a)
+
+#logical opertors
+# print(not True)#opposite of any value
+# a=50
+# b=10
+# # print(not a>b)S
+
+# # val=True
+# # val1=True
+# # print("and operator: ",val and val1)
+# # val2=False
+# print("or operator: ",(a==b) or (a>b))
+
+#type conversion
+# a=2
+# b=2.4
+# a=a+b
+# print(a)
+
+#type casting
+# a=int("2")
+# b=5.1
+# print(type(a))
+# print(a+b)
+
+#input in python
+# v=None
+# v=input("enter your name: " )
+# print(v)
+
+#to convert into expected data type use type casting otherwise all vales would be considered as a string
+# k=int(input(""))
+# print(type(k))
+
+# name=input("enter name: ")
+# age=int(input("enter age: "))
+# marks=float (input("enter marks: "))
+# print("your name is :",name ," your age is :", age, " your marks are :", marks)
+
+
+#practise 1
+# v=float(input("input first number: "))
+# v1=float(input("input second number: "))
+# print("sum of v1 and v :", v1+v)
+
+#practise 2
+# s=float(input("enter side of square: "))
+# print("area of sqare with side: ",s ," is: ",s*s)
+
+#practise 3
+# n1=float(input("enter first number: "))
+# n2=float(input("enter second number"))
+# print("average between ", n1," and ", n2, " is ",(n1+n2)/2)
+
+
+ #practise 4
+n1=int(input("enter first number: "))
+n2=int(input("enter second number"))
+print(n1>=n2)
