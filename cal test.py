7f1c864908b0ce7728ae7fc19ef44e478b005080
MUHAMMAD YOUSAF <yousafaamir205@gamil.com>
2026-06-29 16:49:14 +0500

    modules and its testing

---
+++ cal test.py
@@ -0,0 +1,23 @@
+# import calculator
+# print(calculator.add(10,5))
+# print(calculator.sub(10,5))
+
+# #importing only one function
+
+# from calculator import add
+# print(add(3,4))
+
+# #giving nick name
+
+# import calculator as cal
+# print(cal.add(10,2))
+
+
+#BUILT IN MODULES
+# import math
+# print(math.sqrt(4))#squarre root
+# print(math.pi*2)#pi
+# print(math.cos(20))#cosine function
+
+import random
+print(random.randint(1,10))
